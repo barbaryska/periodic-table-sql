7cce884c12dfd1487e39a61dc1354229532ac0b8
Eugene Lifescale <eugene.lifescale@gmail.com>
2015-07-29 02:17:12 +0300

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `elements` (
+  `element_id` int(11) NOT NULL,
+  `group` tinyint(1) NULL,
+  `period` tinyint(1) NULL,
+  `atomic_number` tinyint(3) NOT NULL,
+  `atomic_mass` decimal(10,8) NOT NULL,
+  `symbol` varchar(2) NOT NULL,
+  `name` varchar(45) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+ALTER TABLE `elements` ADD PRIMARY KEY (`element_id`);
+ALTER TABLE `elements` MODIFY `element_id` INT(11) NOT NULL AUTO_INCREMENT;
